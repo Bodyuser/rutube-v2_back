47cf83f853946a6ff929817237e3a3f8804f772e
Fail <afail1234ahmet@gmail.com>
2023-07-20 17:43:21 +0300

    Update auth decorator

--- src/auth/decorators/auth.decorator.ts
+++ src/auth/decorators/auth.decorator.ts
@@ -4,7 +4,7 @@ import { CheckAuthGuard } from 'src/guards/check-auth.guard'
 import { CheckRoleGuard } from 'src/guards/check-role.guard'
 import { UserRoleEnum } from 'src/users/enums/user-role.enum'
 
-import { convertRoleToNumber } from 'src/utils/ConvertRoleToNumber'
+import { convertRoleToNumber } from 'src/utils/convertRoleToNumber'
 
 export const Auth = (role: UserRoleEnum = UserRoleEnum.USER) => {
 	const numRole = convertRoleToNumber(role)
