1ead287c16626a379ca5aadf706bed87acc72de6
Fail <afail1234ahmet@gmail.com>
2023-07-21 15:44:36 +0300

    Update search resource
    * add get search list endpoint

--- src/search/search.module.ts
+++ src/search/search.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { SearchService } from './search.service';
 import { SearchController } from './search.controller';
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { VideoEntity } from 'src/videos/entities/video.entity'
+import { UserEntity } from 'src/users/entities/user.entity'
 
 @Module({
-  controllers: [SearchController],
-  providers: [SearchService]
+	controllers: [SearchController],
+	providers: [SearchService],
+	imports: [TypeOrmModule.forFeature([VideoEntity, UserEntity])],
 })
 export class SearchModule {}
--- src/search/search.service.ts
+++ src/search/search.service.ts
@@ -1,4 +1,46 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm'
+import { UserEntity } from 'src/users/entities/user.entity'
+import { VideoEntity } from 'src/videos/entities/video.entity'
+import { ILike, MoreThanOrEqual, Not, Repository } from 'typeorm'
 
 @Injectable()
-export class SearchService {}
+export class SearchService {
+	constructor(
+		@InjectRepository(VideoEntity)
+		private videoRepository: Repository<VideoEntity>,
+		@InjectRepository(UserEntity) private userRepository: Repository<UserEntity>
+	) {}
+
+	async getSearchList() {
+		const tags = [
+			...new Set(
+				[].concat(
+					...(
+						await this.videoRepository.find({
+							where: {
+								countViews: MoreThanOrEqual(10),
+							},
+						})
+					).map(video => video.tags)
+				)
+			),
+		].map(item => item.toLowerCase())
+
+		const names = (
+			await this.userRepository.find({
+				where: {
+					followers: MoreThanOrEqual(1),
+					name: Not(ILike('channel%')),
+				},
+				relations: {
+					followers: true,
+				},
+			})
+		).map(user => user.name.toLowerCase())
+
+		const list = [...new Set(tags.concat([...names]))]
+
+		return list.sort((a, b) => a.localeCompare(b))
+	}
+}
